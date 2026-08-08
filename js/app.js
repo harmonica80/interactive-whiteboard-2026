7b70afa599b08bf631ce07bc4073ee9048852722
Teacher <teacher@interactive-whiteboard.local>
2026-08-08 11:53:13 +0800

    Fix: ver 2.3.3 - Use official createTLStore checkValidation false API to eliminate Mount error Boundary crash

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ class App {
     this.dragStart = { x: 0, y: 0 };
     this.imagePos = { x: 0, y: 0 };
     
-    this.APP_VERSION = '2.3.2';
+    this.APP_VERSION = '2.3.3';
     // 初始化狀態快取
     this.questions = [];
     this.images = [];
