129f534d23c697b990eca65c54175dc3edc4f583
harmonica80 <harmonica80@gmail.com>
2026-09-26 08:02:43 +0000

    Deploying to gh-pages from @ harmonica80/interactive-whiteboard-2026@6d83f00ab5d5d21daf3cb5aaf0cbeddb35a6532c 🚀

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ class App {
     this.dragStart = { x: 0, y: 0 };
     this.imagePos = { x: 0, y: 0 };
     
-    this.APP_VERSION = '3.4.9';
+    this.APP_VERSION = '3.5.0';
     this.selectedSongQuizTags = null;
     // 初始化狀態快取
     this.questions = [];
